8424d7dc87d710b986337dfbbe9b9c8bc805c1ad
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-09-22 23:57:44 +0600

    upload to github

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * A Raw Node Js Project To Monitor URl Uptime
+ * Project From Sumit Saha
+ */
+
+// Dependencies
+const http = require('http');
+
+
+// Main Module - Scaffolding
+const app = {};
+
+// Add Configuration
+app.config = {
+    port: 3000
+}
+
+
+// Create 
