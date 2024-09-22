c26734f650ed6c6a0cfbab6b8dfcdecac15809a1
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-09-23 00:53:23 +0600

    request object modified and buffer added

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -2,10 +2,49 @@
  * This is The Helper For Handle Request and Response
  */
 
+// Dependencies
+const { buffer } = require('stream/consumers');
+const {StringDecoder} = require('string_decoder')
+
 const handle = {};
 
 handle.handleReqRes = (req, res) => {
-    res.end("Hello Suckers");
+    
+    // Handle Request
+    const baseURL =  req.protocol + '://' + req.headers.host + '/';
+    const reqUrl = new URL(req.url,baseURL);
+    
+    const pathName = reqUrl.pathname;
+    const trimmedPathName = pathName.replace(/^\/+|\/+$/g, '');
+    const method = req.method;
+    const queryObject = reqUrl.searchParams;
+    const headers = req.headers;
+
+    const requestProperties = {
+        req,
+        reqUrl,
+        pathName,
+        trimmedPathName,
+        method,
+        queryObject,
+        headers
+    }
+
+    const decoder = new StringDecoder('utf-8');
+    let data = '';
+
+    req.on('data', (buffer) => {
+        data += decoder.write(buffer);
+    })
+
+    req.on('end', () => {
+        data += decoder.end();
+
+        console.log(data);
+
+        res.end("Hello Suckers");
+    })
+    
 }
 
 module.exports = handle;
