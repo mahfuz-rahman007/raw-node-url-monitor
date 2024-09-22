895bdc1b72f49745d7baac4acb7d30c68e185c2b
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-09-23 00:22:59 +0600

    index file server created and listen to incoming request

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 // Dependencies
 const http = require('http');
-
+const {handleReqRes} = require('./helpers/handleReqRes');
 
 // Main Module - Scaffolding
 const app = {};
@@ -16,4 +16,16 @@ app.config = {
 }
 
 
-// Create 
+// Create Server
+app.createServer = () => {
+
+    const server = http.createServer(app.handleReqRes);
+
+    server.listen(app.config.port, () => {
+        console.log(`Server Running on port ${app.config.port}`)
+    })
+}
+
+app.handleReqRes = handleReqRes;
+
+app.createServer();
