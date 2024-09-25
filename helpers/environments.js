963a15e58dd363b3a9186a0b380791d2ffebd78d
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-09-26 01:06:21 +0600

    key added for password hashing

--- helpers/environments.js
+++ helpers/environments.js
@@ -9,11 +9,13 @@ const environments = {};
 environments.staging = {
   port: 3000,
   envName: "staging",
+  secretKey: 'gradsgreqarsfsad'
 };
 
 environments.production = {
   port: 5000,
   envName: "production",
+  secretKey: 'dhdtjearhrdhfhsd'
 };
 
 // Determine which environment was passed
