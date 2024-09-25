1033aa1b9694b41c00394b62289fd13aa606458a
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-09-26 01:05:47 +0600

    user route added

--- routes.js
+++ routes.js
@@ -2,10 +2,12 @@
  * Routes For All Js Incoming Request
  */
 
-const {sampleHandler} = require('./handler/routeHandlers/sampleHandler')
+const {sampleHandler} = require('./handler/routeHandlers/sampleHandler');
+const {userHandler} = require('./handler/routeHandlers/userHandler');
 
 const routes = {
-    'sample' : sampleHandler
+    'sample' : sampleHandler,
+    'user': userHandler
 }
 
 module.exports = routes;
